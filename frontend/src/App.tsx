6c1d22e9e15b5f986ca5d89922bf5b28c414e51c
Raúl Ferreyra <raulrferreyrav@gmail.com>
2026-09-17 16:28:38 -0500

    Added new Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroBanner from './components/HeroBanner';
+import BestSellers from './components/BestSellers';
 
 function Home() {
   return (
     <main>
       <HeroBanner />
+      <BestSellers />
     </main>
   );
 }
