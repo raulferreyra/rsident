f57775ad4ca287477aaa0c8d197b3b8d4736a40f
Raúl Ferreyra <raulrferreyrav@gmail.com>
2026-09-17 21:04:34 -0500

    Using type for functional

--- frontend/src/admin/pages/Login.tsx
+++ frontend/src/admin/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
+import type { FormEvent } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
