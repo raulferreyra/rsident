d60db3746f84dd2e26fffe54c1a400237d25e83a
Raúl Ferreyra <raulrferreyrav@gmail.com>
2026-09-17 20:06:57 -0500

    Add footer into the foot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroBanner from './components/HeroBanner';
 import BestSellers from './components/BestSellers';
+import Footerbar from './components/Footerbar';
 
 function Home() {
   return (
@@ -42,6 +43,8 @@ function App() {
         <Route path="/colecciones" element={<Collections />} />
         <Route path="/colecciones/:collection" element={<Collection />} />
       </Routes>
+
+      <Footerbar />
     </BrowserRouter>
   );
 }
